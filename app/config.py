a7b3a49315c970a1cf77113556924d91a5e7f8fc
Thejan2006 <hmagthejanbandar@gmail.com>
2026-09-02 01:09:49 +0530

    feat: refactor DATABASE_URL construction in config.py for improved clarity

--- app/config.py
+++ app/config.py
@@ -4,7 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+    DB_USER: str = os.getenv("DATABASE_USERNAME", "postgres")
+    DB_PASSWORD: str = os.getenv("DATABASE_PASSWORD", "password")
+    DB_HOST: str = os.getenv("DATABASE_HOSTNAME", "localhost")
+    DB_PORT: str = os.getenv("DATABASE_PORT", "5432")
+    DB_NAME: str = os.getenv("DATABASE_NAME", "tasktracker")
+
+    DATABASE_URL: str = os.getenv(
+        "SQLALCHEMY_DATABASE_URL", 
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
